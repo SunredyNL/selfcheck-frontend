fec5e3716e062d21ab1bb0ea8559a222c02857f0
Krystian Jakubczyk <krystianjnl@gmail.com>
2023-10-27 11:32:41 +0200

    Added way to grab user's years

--- src/pages/YearsPage.jsx
+++ src/pages/YearsPage.jsx
@@ -2,61 +2,84 @@ import { useContext, useState, useEffect } from "react";
 import { AuthContext } from "../contexts/AuthContext";
 
 const YearsPage = () => {
-    const token = localStorage.getItem("authToken");
+  const token = localStorage.getItem("authToken");
 
-    const { currentUser } = useContext(AuthContext);
+  const { currentUser } = useContext(AuthContext);
 
-    const [name, setName] = useState("");
-    const payload = {
-        name,
-        user:`${currentUser}` 
-    }
+  const [name, setName] = useState("");
+  const payload = {
+    name,
+    user: `${currentUser}`
+  }
 
-    const addYear = async () => {
-        try {
-          const response = await fetch(
-            `${import.meta.env.VITE_API_URL}/api/year`,
-            {
-              method: "POST",
-              headers: {
-                "Content-Type": "application/json",
-                authorization: `Bearer ${token}`,
-              },
-              body: JSON.stringify(payload),
-            }
-          );
-          if (response.ok) {
-            const currYear = await response.json();
-            console.log(currYear);
-          }
-        } catch (error) {
-          console.log(error);
+  const addYear = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/year`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+          body: JSON.stringify(payload),
         }
-      };
-
-    return (
-        <>
-          <h1>Add year</h1>
-          <form style={{ display: "grid", gridTemplate: "auto / 1fr" }}>
-            <label>
-              Year
-              <input
-                value={name}
-                onChange={(event) => setName(event.target.value)}
-                required
-              />
-            </label>
-            <button
-              type="submit"
-              onClick={() => {
-                addYear();
-              }}
-            >
-              Add year
-            </button>
-          </form>
-        </>
       );
+      if (response.ok) {
+        const currYear = await response.json();
+        console.log(currYear);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const fetchYears = async () => {
+    try {
+      const response = await fetch(
+        `${import.meta.env.VITE_API_URL}/api/year/${currentUser}`,
+        {
+          method: "GET",
+          headers: {
+            "Content-Type": "application/json",
+            authorization: `Bearer ${token}`,
+          },
+        }
+      );
+      if (response.ok) {
+        const allYears = await response.json();
+        console.log("chicorita", allYears);
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => {
+    fetchYears();
+  }, []);
+
+  return (
+    <>
+      <h1>Add year</h1>
+      <form style={{ display: "grid", gridTemplate: "auto / 1fr" }}>
+        <label>
+          Year
+          <input
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+            required
+          />
+        </label>
+        <button
+          type="submit"
+          onClick={() => {
+            addYear();
+          }}
+        >
+          Add year
+        </button>
+      </form>
+    </>
+  );
 }
- 
+
 export default YearsPage;
